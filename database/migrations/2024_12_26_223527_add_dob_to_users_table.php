e60f082e96cae0fb5d7c584d2fd9c93d71ba52c9
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-12-26 22:46:06 +0000

    feat: Add dob column migration

---
+++ database/migrations/2024_12_26_223527_add_dob_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDobToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // gh-ost: ALTER TABLE users ADD COLUMN dob DATE NULL AFTER email
+            $table->date('dob')->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+              $table->dropColumn('dob');
+        });
+    }
+}
