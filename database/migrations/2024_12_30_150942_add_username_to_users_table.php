fbccb805706bcae4affb431b7b316aa6b34ea7f7
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-12-30 15:14:37 +0000

    feat: Update workflow

---
+++ database/migrations/2024_12_30_150942_add_username_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // gh-ost: ALTER TABLE users ADD username VARCHAR(255) NULL after password
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->after('password')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+        });
+        DB::table('migrations')->where('migration', basename(__FILE__, '.php'))->delete();
+    }
+}
