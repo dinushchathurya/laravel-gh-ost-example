8800a6d094e1e7bdf29eb9bce0ef55243987a1ab
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-12-31 11:31:17 +0000

    feat: Update workflow

--- database/migrations/2024_12_31_113026_add_gender_to_users_table.php
+++ database/migrations/2024_12_31_113026_add_gender_to_users_table.php
@@ -12,9 +12,11 @@ class AddGenderToUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   
+        // gh-ost: ALTER TABLE users ADD gender VARCHAR(255) NULL AFTER email;
+        // gh-ost: ALTER TABLE users DROP COLUMN gender; 
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('gender')->nullable()->after('email');
         });
     }
 
@@ -24,9 +26,9 @@ class AddGenderToUsersTable extends Migration
      * @return void
      */
     public function down()
-    {
+    {   
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('gender');
         });
     }
 }
